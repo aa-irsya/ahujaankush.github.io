ee6652589549b7b677be23b77916851b6cbfd1a6
Ankush Ahuja <aahuja2006>
2022-11-01 15:05:32 +0100

    modified:   dist/css/about.min.css 	modified:   dist/css/about.min.css.map 	modified:   dist/css/navbar.min.css 	modified:   dist/css/navbar.min.css.map 	modified:   dist/css/projects.min.css 	modified:   dist/css/projects.min.css.map 	modified:   dist/css/theme.min.css 	modified:   dist/css/theme.min.css.map 	modified:   index.html 	modified:   pages/about.html 	modified:   pages/projects.html 	new file:   src/logic/js/navbar.js 	modified:   src/style/css/about.css 	modified:   src/style/css/about.css.map 	modified:   src/style/css/navbar.css 	modified:   src/style/css/navbar.css.map 	modified:   src/style/css/projects.css 	modified:   src/style/css/projects.css.map 	modified:   src/style/css/theme.css 	modified:   src/style/css/theme.css.map 	modified:   src/style/min.css/about.min.css 	modified:   src/style/min.css/about.min.css.map 	modified:   src/style/min.css/navbar.min.css 	modified:   src/style/min.css/navbar.min.css.map 	modified:   src/style/min.css/projects.min.css 	modified:   src/style/min.css/projects.min.css.map 	modified:   src/style/min.css/theme.min.css 	modified:   src/style/min.css/theme.min.css.map 	modified:   src/style/scss/components/navbar.scss 	modified:   src/style/scss/pages/about.scss 	modified:   src/style/scss/pages/projects.scss 	modified:   src/style/scss/theme.scss

---
+++ src/logic/js/navbar.js
@@ -0,0 +1,6 @@
+let navToggleCont = document.getElementById("nav-toggle")
+
+function navToggle() {
+    if(navToggleCont.style.marginLeft != "0px") navToggleCont.style.marginLeft = "0px"
+    else navToggleCont.style.marginLeft = "-200vw"
+}
