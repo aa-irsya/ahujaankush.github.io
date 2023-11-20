c43f83b70069bc36f9f783c2c7ce0fa54284e23c
Ankush Ahuja <aahuja06@proton.me>
2023-11-20 12:39:54 +0100

    clear

--- src/components/BasicWebContainer.jsx
+++
@@ -1,9 +0,0 @@
-export const BasicWebContainer = ({ sectionID, children }) => {
-  return (
-    <section id={sectionID}>
-      <div className="sec d-block d-flex">
-        <div className="text w-100 h-100 mt-5">{children}</div>
-      </div>
-    </section>
-  );
-};
